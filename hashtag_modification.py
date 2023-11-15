8da73bfe3456c64016ffb9aa4fcf9d6030b6c39f
Cymaphore <x@cymaphore.net>
2023-11-15 18:19:39 +0100

    hashtag_modification.py: Modification tables for hashtags (replacement, blacklisting)

---
+++ hashtag_modification.py
@@ -0,0 +1,14 @@
+##
+# Modification of category hashtags
+
+## Replacement-table for final tags
+## Original -> Replacement
+hashtag_replace = {
+    "#Inland": "#Österreich",
+    
+}
+
+## Blacklist hashtags for categories
+hashtag_blacklist = [
+    "#Bildende"
+]
